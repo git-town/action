22856a286a0f9e0e206920e39636921076ad2b6e
Long Tran <long@connecx.com.au>
2024-11-28 12:17:01 +1100

    remove debug tests

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,6 +1,5 @@
 import { describe, it, beforeEach, expect, vi } from 'vitest'
-import { main, updateDescription } from './main'
-import type { Octokit } from './types'
+import { updateDescription } from './main'
 
 beforeEach(() => {
   vi.unstubAllEnvs()
@@ -51,48 +50,5 @@ describe('updateDescription', () => {
     ].join('\n')
 
     expect(actual).toEqual(expected)
-  })
-})
-
-describe('main', () => {
-  it('should work', async () => {
-    await main({
-      octokit: {} as unknown as Octokit,
-      currentPullRequest: {
-        number: 361,
-        head: {
-          ref: 'test-branch',
-        },
-        base: {
-          ref: 'document-setup',
-        },
-        state: 'open',
-      },
-      pullRequests: [
-        // {
-        //   number: 360,
-        //   head: {
-        //     ref: 'document-setup',
-        //   },
-        //   base: {
-        //     ref: 'main',
-        //   },
-        //   state: 'open',
-        // },
-        {
-          number: 361,
-          head: {
-            ref: 'test-branch',
-          },
-          base: {
-            ref: 'document-setup',
-          },
-          state: 'open',
-        },
-      ],
-      mainBranch: 'main',
-      perennialBranches: [],
-      skipSingleStacks: false,
-    })
   })
 })
